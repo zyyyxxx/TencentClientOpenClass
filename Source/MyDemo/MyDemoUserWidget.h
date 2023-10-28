330b402f887a8f971e43fcf431427dd3d1c87106
zyyyxxx <729494344@qq.com>
2023-10-28 20:25:22 +0800

    Update

---
+++ Source/MyDemo/MyDemoUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MyDemoUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYDEMO_API UMyDemoUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
